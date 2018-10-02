045e2d220278131439dfa32b938be23671baf634
Robin Millette <robin@millette.info>
2018-10-02 19:37:33 -0400

    404 page.

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+// npm
+import React from 'react'
+
+// self
+import { Footer, Nav } from '../components'
+
+export default (props) => (
+  <div>
+    <Nav />
+    <p>Nothing here!</p>
+    <Footer />
+  </div>
+)
