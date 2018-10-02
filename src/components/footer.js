3825ae59e6a7e8090dfcd3b9d2b51c7815673182
Robin Millette <robin@millette.info>
2018-10-02 16:40:59 -0400

    Satisfy linter (standardjs).

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,39 +5,39 @@ import React from 'react'
 import img from '../files/img/gare-sticky-blue.png'
 
 export default (props) => (
-  <div style={{ display: "flex" }}>
-    <div style={{ flex: "auto" }}>
+  <div style={{ display: 'flex' }}>
+    <div style={{ flex: 'auto' }}>
       <h4>Contact</h4>
-      <ul className="list-unstyled contact">
-        <li className="meetup"><a href="http://meetup.com/js-montreal">meetup.com</a></li>
-        <li className="facebook"><a href="https://www.facebook.com/groups/253683934748003/">facebook group</a></li>
-        <li className="linkedin"><a href="http://www.linkedin.com/groups?gid=2514289">js-montreal.org</a></li>
-        <li className="twitter"><a href="http://www.twitter.com/jsmontreal">@jsmontreal</a></li>
-        <li className="email"><a href="mailto:contact@js-montreal.org" target="_blank">Email us</a></li>
+      <ul className='list-unstyled contact'>
+        <li className='meetup'><a target='_blank' rel='noopener noreferrer' href='http://meetup.com/js-montreal'>meetup.com</a></li>
+        <li className='facebook'><a target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/groups/253683934748003/'>facebook group</a></li>
+        <li className='linkedin'><a target='_blank' rel='noopener noreferrer' href='http://www.linkedin.com/groups?gid=2514289'>js-montreal.org</a></li>
+        <li className='twitter'><a target='_blank' rel='noopener noreferrer' href='http://www.twitter.com/jsmontreal'>@jsmontreal</a></li>
+        <li className='email'><a target='_blank' rel='noopener noreferrer' href='mailto:contact@js-montreal.org'>Email us</a></li>
       </ul>
     </div>
-    <div style={{ flex: "auto" }}>
+    <div style={{ flex: 'auto' }}>
       <h4>User Groups</h4>
-      <ul className="list-unstyled">
+      <ul className='list-unstyled'>
         <li>
-          <a href="http://gtugmontreal.blogspot.ca">GDG Montreal</a>
+          <a target='_blank' rel='noopener noreferrer' href='http://gtugmontreal.blogspot.ca'>GDG Montreal</a>
         </li>
         <li>
-          <a href="http://www.montrealonrails.com/">montreal.rb</a>
+          <a target='_blank' rel='noopener noreferrer' href='http://www.montrealonrails.com/'>montreal.rb</a>
         </li>
         <li>
-          <a href="http://www.phpquebec.org/">php quebec</a>
+          <a target='_blank' rel='noopener noreferrer' href='http://www.phpquebec.org/'>php quebec</a>
         </li>
         <li>
-          <a href="http://montrealpython.org/">mtl python</a>
+          <a target='_blank' rel='noopener noreferrer' href='http://montrealpython.org/'>mtl python</a>
         </li>
       </ul>
     </div>
-    <div style={{ flex: "auto" }}>
+    <div style={{ flex: 'auto' }}>
       <h4>Special thanks to</h4>
-      <ul className="list-unstyled">
+      <ul className='list-unstyled'>
         <li>
-          <a href="https://garemtl.com/en/"><img alt="La Gare" src={img} width="100" /></a>
+          <a target='_blank' rel='noopener noreferrer' href='https://garemtl.com/en/'><img alt='La Gare' src={img} width='100' /></a>
         </li>
       </ul>
     </div>
--- src/components/nav.js
+++ src/components/nav.js
@@ -4,22 +4,22 @@ import { Link } from 'gatsby'
 
 export default (props) => (
   <header>
-    <div className="container">
-      <nav role="navigation">
-        <ul className="nav-simple">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/archive">Archive</Link></li>
-          <li><Link to="/presenter">Present</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><a href="http://www.meetup.com/js-montreal/messages/boards/thread/48462382">Jobs</a></li>
-          <li><a href="http://goo.gl/forms/JXCnE4K9bG">2014 Survey</a></li>
-          <li><strong><a href="http://goo.gl/forms/5U4gfTIXzt">Join the team!</a></strong></li>
+    <div className='container'>
+      <nav role='navigation'>
+        <ul className='nav-simple'>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/archive'>Archive</Link></li>
+          <li><Link to='/presenter'>Present</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><a target='_blank' rel='noopener noreferrer' href='http://www.meetup.com/js-montreal/messages/boards/thread/48462382'>Jobs</a></li>
+          <li><a target='_blank' rel='noopener noreferrer' href='http://goo.gl/forms/JXCnE4K9bG'>2014 Survey</a></li>
+          <li><strong><a target='_blank' rel='noopener noreferrer' href='http://goo.gl/forms/5U4gfTIXzt'>Join the team!</a></strong></li>
         </ul>
       </nav>
-      <div className="row">
-        <div className="col-md-12">
+      <div className='row'>
+        <div className='col-md-12'>
           <h1>
-            <Link href="/">js-montreal</Link><br />
+            <Link href='/'>js-montreal</Link><br />
             <small>Montreal's JavaScript User Group</small>
           </h1>
         </div>
