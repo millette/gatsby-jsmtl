3594dc4c5ba240d9d86a89c7b308a9f30eb2b5a0
Robin Millette <robin@millette.info>
2018-10-03 18:23:29 -0400

    Allow html values.

--- src/components/event.js
+++ src/components/event.js
@@ -5,11 +5,10 @@ import React from 'react'
 import { Session } from '.'
 import { date } from '../utils'
 
-export default ({ num, title, on, blurb, specialevent, speakers }) => (
+export default ({ num, title, on, blurb, speakers }) => (
   <div key={`num-${num}`}>
-    <h2 style={{ marginBottom: 0 }}>#{num} {title} on <small>{date(on)}</small></h2>
-    <p>{blurb}</p>
-    {specialevent && specialevent.publicURL && <a href={specialevent.publicURL}>special</a>}
+    <h2 style={{ marginBottom: 0 }} dangerouslySetInnerHTML={{ __html: `#${num} ${title} on <small>${date(on)}</small>` }} />
+    <p dangerouslySetInnerHTML={{ __html: blurb }} />
     <div style={{ display: 'flex' }}>
       {speakers.map((speaker, i) => (
         <Session id={`speaker-${num}-${i}`} key={`speaker-${num}-${i}`} {...speaker} />
--- src/components/session.js
+++ src/components/session.js
@@ -5,6 +5,6 @@ export default ({ id, title, name, url, email, synopsis }) => (
   <div id={id} style={{ paddingRight: '1rem', flex: '1 1 0' }}>
     <h3>{title}</h3>
     <h4>by {name}</h4>
-    <p>{synopsis}</p>
+    <p dangerouslySetInnerHTML={{ __html: synopsis }} />
   </div>
 )
