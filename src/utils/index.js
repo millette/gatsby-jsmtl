5e06c4233b7b453daac3e5e9d52e65382230b742
Robin Millette <robin@millette.info>
2018-10-03 16:59:19 -0400

    Make utils commonjs.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,15 @@
-const date = (dateStr) => {
-  const year = parseInt(dateStr.slice(0, 4), 10)
-  const month = parseInt(dateStr.slice(4, 6), 10) - 1
-  const day = parseInt(dateStr.slice(6, 8), 10)
-  return new Date(year, month, day, 12).toDateString()
-}
-
-export {
-  date
+module.exports = {
+  date: (dateStr) => new Date(
+    parseInt(dateStr.slice(0, 4), 10),
+    parseInt(dateStr.slice(4, 6), 10) - 1,
+    parseInt(dateStr.slice(6, 8), 10),
+    12
+  ).toDateString(),
+  yearRange: (first, last) => {
+    const firstYear = parseInt(first.slice(0, 4), 10)
+    return Array(parseInt(last.slice(0, 4), 10) - firstYear + 1)
+      .fill()
+      .map((a, i) => i + firstYear)
+      .reverse()
+  }
 }
